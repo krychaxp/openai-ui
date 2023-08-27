06f9488afeba6a5455eca5e4a48b72273f146f5a
krychaxp <krychaxp@op.pl>
2023-08-27 18:29:23 +0200

    🔧 fix(layout.tsx): add Google Analytics script to the head of the HTML document to enable tracking of website traffic 🚀 feat(layout.tsx): add support for configuring Google Analytics tracking ID through the NEXT_PUBLIC_GOOGLE_GTAG environment variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 
 import { AppProviders } from './AppProviders';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
+
+const NEXT_PUBLIC_GOOGLE_GTAG = 'G-QQYM9S6SDQ';
 
 export default function RootLayout({
   children,
@@ -12,6 +15,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${NEXT_PUBLIC_GOOGLE_GTAG}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${NEXT_PUBLIC_GOOGLE_GTAG}');
+        `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <AppProviders>{children}</AppProviders>
       </body>
